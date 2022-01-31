694e0c8c5c115973e2107175529a9071597a33c3
Gilberto Medeiros <Gilberto Medeiros>
2022-01-30 21:16:05 -0300

    Alteração no profile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,9 +28,8 @@
 //     )
 // }
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Disclosure, Menu } from '@headlessui/react'
+import {  MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const navigation = [
   { name: 'Projetos', href: '#projects', current: false },

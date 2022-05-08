6bd72ec7942906a174c9a0410b2110bdb993a063
Gilberto Medeiros <62359000+gilbertomoj@users.noreply.github.com>
2022-05-08 17:39:29 -0300

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,7 @@ export default function Skills() {
         <div className="text-center mb-20">
           <ChipIcon className="w-10 inline-block mb-4 mt-10"  />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Tecnologias
+            Tecnologies
           </h1>
 
         </div>
